4f139b8f457c678a17c3933cd1cf1cefb1632f8d
bijita <bijitalama0323@gmail.com>
2020-10-12 19:20:44 +0545

    learned about generic

---
+++ csharpbasics/Generic.cs
@@ -0,0 +1,13 @@
+public class Calculator
+{
+    public static int Sum(int firstNum, int secondNum)
+    {
+        return firstNum + secondNum;
+    }
+    //using generic method we can work on multiple data type
+    public static int Sum<T>(T firstNum, T secondNum)
+    {
+        return firstNum;//only syntax
+
+    }
+}
